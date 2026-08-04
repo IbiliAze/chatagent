fa4813bf599168a102d1e6914bce096fc8cde500
Ibi Hasanli <ibili73@gmail.com>
2026-08-04 21:07:11 +0100

    pyproject: add pylint dev dependency; add RAG integration test stubs

---
+++ tests/rag/integration/test_rag.py
@@ -0,0 +1,45 @@
+import pytest
+from dotenv import load_dotenv
+from langchain_community.vectorstores import OpenSearchVectorSearch
+from langchain_core.documents import Document
+
+from app.rag.rag import Rag
+from core.store.vectorstore.opensearch import OpenSearch
+
+load_dotenv()
+
+pytestmark = pytest.mark.integration
+
+
+@pytest.fixture
+def vector_store() -> OpenSearchVectorSearch:
+  opensearch = OpenSearch()
+  opensearch.provision_indexes(embedding_dimension=1536)
+  return opensearch.document_vectorstore
+
+
+@pytest.fixture
+def rag(vector_store: OpenSearchVectorSearch) -> Rag:
+  return Rag(vector_store)
+
+
+class TestAddition:
+  def test_add_documents_returns(self, rag: Rag) -> None:
+    number_of_chunks = rag.add_documents(
+      documents=[Document(page_content='My test document')],
+      source='Test document source',
+    )
+
+    assert number_of_chunks is not None
+    assert type(number_of_chunks) is int
+    assert number_of_chunks == 1
+
+  def test_add_texts_returns(self, rag: Rag) -> None:
+    number_of_chunks = rag.add_texts(texts=['My small text'], source='Test text source')
+
+    assert number_of_chunks is not None
+    assert type(number_of_chunks) is int
+    assert number_of_chunks == 1
+
+
+#   def test_ask_returns_relevant_info(self, rag:Rag)
